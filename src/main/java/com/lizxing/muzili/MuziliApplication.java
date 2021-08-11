e739c49257ca6024a62b3143bf4cbfc53c413005
lizxing <1483588183@qq.com>
2021-08-11 16:43:22 +0800

    0811

---
+++ src/main/java/com/lizxing/muzili/MuziliApplication.java
@@ -0,0 +1,13 @@
+package com.lizxing.muzili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuziliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lizxing/muzili/MuziliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizxing.muzili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
